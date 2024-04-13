d79ee3cbe339ca4625acfb0379b563b36eb7044e
Çağan <cagangdk6@gmail.com>
2024-04-13 22:33:49 +0300

    Update basic_pass_alg.py

--- basic_pass_alg.py
+++ basic_pass_alg.py
@@ -3,7 +3,6 @@ import random
 uzunluk=int(input("Enter lenght of password which you want to create: "))
 password=""
 for i in range(0,uzunluk):
-    tek_cift=random.randint(0,10)
     rand=random.randint(33,126)
     password=password+chr(rand)
 print(f"Your password is:{password}")
